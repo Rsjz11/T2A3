cd0c9c499e40bcb4ffd0514b9d77c52213b87a7c
Ray Jewell <rayjewell268@gmail.com>
2020-10-02 01:20:50 +1000

    add commas into the text and changed the conversion specifier values into a day, month, year and time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from datetime import datetime
 # Figure out how to use URL
 global_url = 'https://api.alternative.me/v2/global/'
 
@@ -20,9 +21,12 @@ active_markets_string = '{:,}'.format(active_markets)
 global_cap_string = '{:,}'.format(global_cap)
 global_volume_string = '{:,}'.format(global_volume)
 
+# strftime is a python built in module that changes a certain value by adding all the conversion specifier values. The value is the timestamp that will be converted to a calender date & time. 
+last_updated_string = datetime.fromtimestamp(last_updated).strftime('%d %B, %Y at %I:%M%p ')
+
 print()
 print('There are currently ' + active_currencies_string + ' active cryptocurrencies and ' + active_markets_string + ' active markets.')
 print('The global cap of all cryptocurrency is ' + global_cap_string + ' and the 24h global volume is ' + global_volume_string + '.')
-print('Bitcoin\'s total percentage of the total global cap is ' + str(bitcoin_percentage) + '%.')
+print('Bitcoin\'s total percentage of the total global cap is ' + str(bitcoin_percentage) + ' %.')
 print()
-print('This information was last updated on ' + str(last_updated) + '.')
+print('This information was last updated on ' + last_updated_string + '.')
