77b62c7a18d80b8e23118eb929df4e1651801d51
Ray Jewell <rayjewell268@gmail.com>
2020-10-02 00:57:00 +1000

    Second commit: Ready to replace the str with updated variable strings

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ results = request.json()
 active_currencies = results['data']['active_cryptocurrencies']
 active_markets = results['data']['active_markets']
 bitcoin_percentage = results['data']['bitcoin_percentage_of_market_cap']
-last_update = results['data']['last_updated']
+last_updated = results['data']['last_updated']
 global_cap = results['data']['quotes']['USD']['total_market_cap']
 global_volume = results['data']['quotes']['USD']['total_volume_24h']
 
+active_currencies_string = '{;,}'.format(active_currencies)
+active_markets_string = '{;,}'.format(active_markets)
+global_cap_string = '{;,}'.format(global_cap)
+global_volume_string = '{;,}'.format(global_volume)
+
 print()
-print('There are currently ' + str(active_currencies) + ' activecryptocurrencies and' + str(active_markets) + ' active markets.')
+print('There are currently ' + str(active_currencies) + ' active cryptocurrencies and ' + str(active_markets) + ' active markets.')
+print('The global cap of all cryptocurrency is ' + str(global_cap) + ' and the 24h global volume is ' + str(global_volume) + '.')
+print('Bitcoin\'s total percentage of the total global cap is ' + str(bitcoin_percentage) + '%.')
+print()
+print('This information was last updated on ' + str(last_updated) + '.')
