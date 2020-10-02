036232b4fd113ed1683b0c9b0552a8bb93234294
Ray Jewell <rayjewell268@gmail.com>
2020-10-02 11:20:48 +1000

    Completed the list of cryptocurrency on the Cryptocurrency_list file

--- Cryptocurrency_list.py
+++ Cryptocurrency_list.py
@@ -6,4 +6,12 @@ listing_url = 'https://api.alternative.me/v2/listings/'
 request = requests.get(listing_url)
 results = request.json()
 
-print(json.dumps(results, sort_keys=True, indent=4))
+#print(json.dumps(results, sort_keys=True, indent=4))
+
+data = results['data']
+
+for currency in data: 
+    rank = currency['id']  
+    name = currency['name']
+    symbol = currency['symbol']
+    print(str(rank) + ': ' + name + ' (' + symbol + ')')
