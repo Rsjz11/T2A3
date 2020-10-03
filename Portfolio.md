633e1466027aa0a929b77a1cd1cb3013c1fb80f7
Ray Jewell <rayjewell268@gmail.com>
2020-10-03 14:24:40 +1000

    Basic form done

--- Portfolio.md
+++ Portfolio.md
@@ -1,3 +1,5 @@
 btc 2
 eth 20
 neo 45
+ltc 20
+rdn 10000
--- classes_and_functions.py
+++ classes_and_functions.py
@@ -2,3 +2,8 @@ def json_request():
     request = requests.get(ticker_url)
     results = request.json()
 
+def accepted_currency():
+    currency = input("Please enter one of listed currencies: USD, EUR, GBP, RUB, JPY, CAD, KRW, PLN")
+    if currency ==  USD', 'EUR', 'GBP', 'RUB', 'JPY', 'CAD', 'KRW', 'PLN':
+        continue
+
