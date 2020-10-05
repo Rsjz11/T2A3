b859a0d76891ce97d623f6037ba10ab0e5c801c5
Ray Jewell <rayjewell268@gmail.com>
2020-10-05 15:11:54 +1100

    Adjusted PNG file

---
+++ src/test/test.py
@@ -0,0 +1,8 @@
+import unittest
+from Portfolio import portfolio
+
+# Test whether or not the portfolio accurately caluculates the asset price.
+# Test whether the API is accurately retrieved from the corressponding API
+
+
+
